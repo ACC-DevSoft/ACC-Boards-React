2d5011c2debfab79daed2b8e507713792142e72a
Jalexss <jhalx2198@gmail.com>
2021-07-19 19:25:48 -0500

    Fix README and AccBoardsApp in index.js

---
+++ frontend/src/AccBoardsApp.js
@@ -0,0 +1,11 @@
+import './AccBoards.css';
+
+function AccBoardsApp() {
+	return (
+		<div>
+			<h1>Acc Boards App</h1>
+		</div>
+	);
+}
+
+export default AccBoardsApp;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AccBoardApp from './AccBoardApp';
+import AccBoardsApp from './AccBoardsApp';
 
 import './index.css';
 
 ReactDOM.render(
-	<AccBoardApp />,
+	<AccBoardsApp />,
 
 	document.getElementById('root')
 );
