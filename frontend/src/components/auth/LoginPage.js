8c43dd5e78b39dc0e5ffec048a062dde717401cc
Duvan Villadiego <duvan.villadiego.oficial@gmail.com>
2021-07-21 21:50:27 -0500

    S

--- frontend/src/components/auth/LoginPage.js
+++ frontend/src/components/auth/LoginPage.js
@@ -1,40 +1,57 @@
 import React, { useState } from "react";
 import { Modal, TextField, Button } from "@material-ui/core";
+import Card from '@material-ui/core/Card';
 import "./loginPage.css";
+import logo from "../../assets/img/Acc-logo_Purple.png";
+
+async function fetchLoginApi(state) {
+  const data = await fetch("http://localhost:3025/api/auth/login", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(state),
+    }).then(response => response.json()).then(json => {console.log(json)}).catch(err => {return err});
+};
 
 const LoginPage = () => {
-  const url = "http://localhost:3000/api/auth/login";
-  const fetchLoginApi = async () => {
-    const response = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        email: state.email,
-        password: state.password,
-      }),
-    });
-    const data = await response.json();
-    return data;
-  };
+
   const [modal, setModal] = useState(false);
-  let state = {email: "", password: ""};
+
+  const abrirModal = () => {
+    setModal(!modal);
+  };
+
+  const state = {email: "", password: ""};
+
   const valueToStateEmail = (email) => {
     state.email = email.value;
     console.log(state);
   }
   const valueToStatePassword = (password) => {
     state.password = password.value;
-    console.log(state);
+    console.log(state); 
   }
-  const abrirModal = () => {
-    setModal(!modal);
-  };
+
   const LoginForm = (
-    <form >
-      <input name="email" type="text" placeholder="email" onChange={event => valueToStateEmail(event.target)}/>
-      <input name="email" type="text" placeholder="email" onChange={event => valueToStatePassword(event.target)}/>
-      <Button onClick={() => fetchLoginApi()}>Login</Button>
-    </form>
+    <Card className="element">
+      <h1>Login</h1>
+      <hr className="spacer"></hr>
+      <div className="content-img">
+        <img className="Img" src={logo}/>
+      </div>
+      <form className="form-content-inputs">
+        <TextField name="email" type="text" placeholder="email" onChange={event => valueToStateEmail(event.target)}/>
+        <TextField name="password" type="text" placeholder="password" onChange={event => valueToStatePassword(event.target)}/>
+      </form>
+      <div className="mat-dialog-actions">
+        <span>Create an account <a onClick={() => setModal(false)} href="#">Sign Up</a></span>
+        <Button className="btn" onClick={() => fetchLoginApi(state)}>Login</Button>
+      </div>
+    </Card >
+    
   );
+
   return (
     <section>
       <Modal open={modal} onClose={() => setModal(false)}>
