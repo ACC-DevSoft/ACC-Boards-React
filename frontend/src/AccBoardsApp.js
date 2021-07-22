7a224c577ab883dfd2a60ea91827566ee2b2d63d
Duvan Villadiego <duvan.villadiego.oficial@gmail.com>
2021-07-22 11:34:07 -0500

    S

--- frontend/src/AccBoardsApp.js
+++ frontend/src/AccBoardsApp.js
@@ -1,10 +1,11 @@
 import './AccBoards.css';
 import React from 'react';
+import AddWorkSpaces from './components/workspaces/AddWorkSpaces';
 import LoginPage from './components/auth/LoginPage';
 
 function AccBoardsApp() {
 	return (
-		<LoginPage/>
+		<LoginPage />
 	);
 };
 
