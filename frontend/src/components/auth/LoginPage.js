1746b69f21cccb097841469c5be674c855395d75
Duvan Villadiego <duvan.villadiego.oficial@gmail.com>
2021-07-22 11:33:40 -0500

    s

--- frontend/src/components/auth/LoginPage.js
+++ frontend/src/components/auth/LoginPage.js
@@ -1,55 +1,125 @@
 import React, { useState } from "react";
-import { Modal, TextField, Button } from "@material-ui/core";
-import Card from '@material-ui/core/Card';
+import {
+  Modal,
+  TextField,
+  Button,
+  IconButton,
+  Collapse,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import Card from "@material-ui/core/Card";
 import "./loginPage.css";
 import logo from "../../assets/img/Acc-logo_Purple.png";
+import Alert from "@material-ui/lab/Alert";
 
-async function fetchLoginApi(state) {
-  const data = await fetch("http://localhost:3025/api/auth/login", {
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(state),
-    }).then(response => response.json()).then(json => {console.log(json)}).catch(err => {return err});
-};
+const state = { email: "", password: "" };
 
 const LoginPage = () => {
-
   const [modal, setModal] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  async function fetchLoginApi(state) {
+    await fetch("http://localhost:3025/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(state),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.token) {
+          localStorage.setItem("token", json.token);
+          setModal(false);
+        } else {
+          errorSetter(json.message)
+        }
+      })
+      .catch((err) => setErrorMessage("Server error"));
+  }
+
+  function verifyData(state) {
+    if (state.email === "" || state.password === "") {
+      errorSetter("Please enter your email and password");
+    } else {
+      fetchLoginApi(state);
+    }
+  }
+
+  function errorSetter(state) {
+    setError(!error);
+    setErrorMessage(state);
+    setInterval(() => {
+      setError(false);
+    }, 3000);
+  }
 
   const abrirModal = () => {
     setModal(!modal);
   };
 
-  const state = {email: "", password: ""};
-
   const valueToStateEmail = (email) => {
     state.email = email.value;
     console.log(state);
-  }
+  };
   const valueToStatePassword = (password) => {
     state.password = password.value;
-    console.log(state); 
-  }
+    console.log(state);
+  };
 
   const LoginForm = (
     <Card className="element">
       <h1>Login</h1>
       <hr className="spacer"></hr>
       <div className="content-img">
-        <img className="Img" src={logo}/>
+        <img className="Img" src={logo} alt="Acc-Board logo" />
       </div>
+      <Collapse in={error}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setError(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          severity="error"
+        >
+          {errorMessage}
+        </Alert>
+      </Collapse>
       <form className="form-content-inputs">
-        <TextField name="email" type="text" placeholder="email" onChange={event => valueToStateEmail(event.target)}/>
-        <TextField name="password" type="text" placeholder="password" onChange={event => valueToStatePassword(event.target)}/>
+        <TextField
+          name="email"
+          type="text"
+          placeholder="email"
+          onChange={(event) => valueToStateEmail(event.target)}
+        />
+        <TextField
+          name="password"
+          type="text"
+          placeholder="password"
+          onChange={(event) => valueToStatePassword(event.target)}
+        />
       </form>
       <div className="mat-dialog-actions">
-        <span>Create an account <a onClick={() => setModal(false)} href="#">Sign Up</a></span>
-        <Button className="btn" onClick={() => fetchLoginApi(state)}>Login</Button>
+        <span>
+          Create an account{" "}
+          <a onClick={() => setModal(false)} href="#">
+            Sign Up
+          </a>
+        </span>
+        <Button className="btn" onClick={() => verifyData(state)}>
+          Login
+        </Button>
       </div>
-    </Card >
-    
+    </Card>
   );
 
   return (
