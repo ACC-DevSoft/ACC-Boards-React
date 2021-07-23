54b188e6fdf6ffa98c157d0034770f11e0273299
Duvan Villadiego <duvan.villadiego.oficial@gmail.com>
2021-07-23 10:44:53 -0500

    feat: role register added

--- frontend/src/AccBoardsApp.js
+++ frontend/src/AccBoardsApp.js
@@ -1,14 +1,15 @@
 import './AccBoards.css';
 import React from 'react';
-// import AddWorkSpaces from './components/workspaces/AddWorkSpaces';
 import LoginPage from './components/auth/LoginPage';
 import AddUser from './components/admin/AddUser';
+import RegisterRole from './components/admin/RegisterRole';
 
 function AccBoardsApp() {
 	return (
 		<section>
 			<AddUser />
 			<LoginPage />
+			<RegisterRole />
 		</section>
 	);
 };
