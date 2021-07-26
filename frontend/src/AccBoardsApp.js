6999a5981e9c3ef6a20d2bd7771dc4f8cf7a4621
Jalexss <jhalx2198@gmail.com>
2021-07-26 11:54:05 -0500

    Fixe Navbar phase 1

--- frontend/src/AccBoardsApp.js
+++ frontend/src/AccBoardsApp.js
@@ -1,47 +1,18 @@
 import React from 'react';
 import './AccBoards.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Navbar from "./components/home/Navbar";
-import Home from "./components/home/Home";
-import ListWorkSpaces from "./components/workspaces/ListWorkSpaces";
-import Profile from './components/users/Profile';
-// import ListWorkSpaces from "./components/workspaces/ListWorkSpaces";
-import ListRole from './components/admin/ListRole';
-import ListUsers from "./components/admin/ListUsers";
-import LoginPage from './components/auth/LoginPage';
-import RegisterPage from './components/auth/RegisterPage';
-<<<<<<< HEAD
-import AddUser from './components/admin/AddUser';
+
 import AppRouter from './routers/AppRouter';
-
-
-function AccBoardsApp() {
-	return (
-		<AppRouter />
-	)
-}
-=======
+import UserContext from './UserContext';
 
 function AccBoardsApp() {
+	const userLogged = {
+		login: false,
+		Admin: false,
+	};
 	return (
-		<>
-		<Router>
-			<Navbar />
-			<Switch>
-				<Route path="/" exact><Home/></Route>
-				<Route path="/WorkSpaces" ><ListWorkSpaces/></Route>
-				<Route path="/Profile" ><Profile/></Route>
-				<Route path="/ListRole" ><ListRole/></Route>
-				<Route path="/ListUsers" ><ListUsers/></Route>
-				<Route path="/register" ><RegisterPage/></Route>
-				<Route path="/Login" ><LoginPage/></Route>
-			</Switch>
-		</Router>
-		</>
+		<UserContext.Provider value={userLogged}>
+			<AppRouter />
+		</UserContext.Provider>
 	);
-};
-
-
->>>>>>> develop
+}
 export default AccBoardsApp;
-
--- frontend/src/components/home/Navbar.js
+++ frontend/src/components/home/Navbar.js
@@ -1,37 +1,128 @@
-import React from "react";
-import {AppBar, Toolbar} from '@material-ui/core';
-import {Link} from 'react-router-dom';
-import logo from "../../assets/img/Acc_Logo2.png";
-import LoginPage from "../auth/LoginPage";
-import "./Styles.css";
+import React, { useContext } from 'react';
+import {
+	AppBar,
+	Button,
+	Toolbar,
+	Typography,
+	makeStyles,
+} from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/img/Acc_Logo2.png';
+import LoginPage from '../auth/LoginPage';
+import './Styles.css';
+import UserContext from '../../UserContext';
 
-export default function Navbar (){
-    return(
-        <AppBar position="static" >
-        <Toolbar className="bgHeader">
-            <div className="logo">
-            <Link to="/">
-                <img  src={logo} alt="logo" className="img" />
-                {/* <Typography>ACC-BOARDS</Typography> */}
-            </Link>
-            </div>
-            <div className="nav-items">
-                <Link  className="btn" to="/workSpaces">WorkSpaces</Link>
-                <Link  className="btn" to="/ListUsers">ListUsers</Link>
-                <Link  className="btn" to="/ListRole">ListRole</Link>
-            </div>
-                <Link className="btn-stroked">
-                    <LoginPage className="btn-stroked" color="secundary"/>    
-                </Link>
-            <div className="nav-items">
-                <Link className="btn" to="/Profile">Profile</Link>
-                <Link className="btn-stroked" to="/register">Sign Up</Link>
-                {/* <Link className="btn-stroked"> */}
-                {/* <LoginPage/>  */}
-                {/* </Link> */}
-                {/* <Link className="btn-stroked" to="/Login">Login</Link> */}
-            </div>
-        </Toolbar>
-    </AppBar>
-    );
+const useStyles = makeStyles({
+	root: {
+		color: 'white',
+		padding: '8px',
+		marginRight: '5px',
+		border: 'white',
+		'&:hover': {
+			backgroundColor: 'trasnparent',
+			border: '1px solid white',
+			borderRadius: '0.5rem',
+			color: 'white',
+			textDecoration: 'none',
+		},
+	},
+});
+
+export default function Navbar() {
+	const classes = useStyles();
+
+	const userContext = useContext(UserContext);
+
+	console.log(userContext);
+
+	return (
+		<AppBar position="static">
+			<Toolbar className="bgHeader">
+				<div className="logo">
+					<NavLink to="/">
+						<img src={logo} alt="logo" className="img" />
+						{/* <Typography>ACC-BOARDS</Typography> */}
+					</NavLink>
+				</div>
+				<div className="nav-items">
+					{userContext.login && (
+						<>
+							<NavLink
+								exact
+								activeClassName="active"
+								className={classes.root}
+								to="/workSpaces"
+							>
+								WorkSpaces
+							</NavLink>
+							<NavLink
+								exact
+								activeClassName="active"
+								className={classes.root}
+								to="/ListUsers"
+							>
+								ListUsers
+							</NavLink>
+							<NavLink
+								exact
+								activeClassName="active"
+								className={classes.root}
+								to="/ListRole"
+							>
+								ListRole
+							</NavLink>
+						</>
+					)}
+				</div>
+				<div className="nav-items">
+					{userContext.login && (
+						<NavLink
+							exact
+							activeClassName="active"
+							className={classes.root}
+							to="/Profile"
+						>
+							Profile
+						</NavLink>
+					)}
+
+					{!userContext.login && (
+						<>
+							<NavLink
+								exact
+								activeClassName="active"
+								className={`btn-stroked  ${classes.root}`}
+								to="/register"
+							>
+								Sign Up
+							</NavLink>
+							<NavLink
+								exact
+								activeClassName="active"
+								className={`btn-stroked  ${classes.root}`}
+								to="/login"
+							>
+								Login
+							</NavLink>
+						</>
+					)}
+					{userContext.login && (
+						<NavLink
+							exact
+							activeClassName="active"
+							className={`btn-stroked  ${classes.root}`}
+							to="/logout"
+						>
+							Logout
+						</NavLink>
+					)}
+
+					{/* <span ><LoginPage /></span> */}
+					{/* <NavLink className="btn-stroked"> */}
+					{/* </NavLink> */}
+					{/* <NavLink className={`btn-stroked  ${classes.root}`} to="/Login">Login</NavLink> */}
+				</div>
+			</Toolbar>
+		</AppBar>
+	);
 }
