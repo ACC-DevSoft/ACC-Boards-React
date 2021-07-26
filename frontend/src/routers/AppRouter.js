fb14abc881e53e494f4816abd0a95953f3b0069f
Jalexss <jhalx2198@gmail.com>
2021-07-25 21:54:15 -0500

    Fix Approuting

--- frontend/src/routers/AppRouter.js
+++ frontend/src/routers/AppRouter.js
@@ -6,15 +6,44 @@ import {
 	Redirect,
 } from 'react-router-dom';
 
+import Navbar from '../components/home/Navbar';
+import Home from '../components/home/Home';
+import ListRole from '../components/admin/ListRole';
+import ListUsers from '../components/admin/ListUsers';
+import LoginPage from '../components/auth/LoginPage';
+import RegisterPage from '../components/auth/RegisterPage';
+import Profile from '../components/users/Profile';
+import ListWorkSpaces from '../components/workspaces/ListWorkSpaces';
+
 const AppRouter = () => {
 	return (
 		<Router>
 			<div className="">
 				<div className="">
+					<Navbar />
 					<Switch>
-						<Route />
-
-						<Redirect />
+						<Route path="/WorkSpaces">
+							<ListWorkSpaces />
+						</Route>
+						<Route path="/Profile">
+							<Profile />
+						</Route>
+						<Route path="/ListRole">
+							<ListRole />
+						</Route>
+						<Route path="/ListUsers">
+							<ListUsers />
+						</Route>
+						<Route path="/register">
+							<RegisterPage />
+						</Route>
+						<Route path="/Login">
+							<LoginPage />
+						</Route>
+						<Route path="/" exact>
+							<Home />
+						</Route>
+						<Redirect to="/" />
 					</Switch>
 				</div>
 			</div>
